b43e44a89dcfb038de0f06ae95ff9e79a7497a06
sharkinsspatial <sharkinsgis@gmail.com>
2022-07-11 17:53:30 -0500

    First

---
+++ terraform/variables.tf
@@ -0,0 +1,32 @@
+variable "project" {
+  type = string
+}
+
+variable "credentials_file" {
+  type = string
+}
+
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "zone" {
+  type    = string
+  default = "us-central1-c"
+}
+
+variable "gke_username" {
+  default     = ""
+  description = "gke username"
+}
+
+variable "gke_password" {
+  default     = ""
+  description = "gke password"
+}
+
+variable "gke_num_nodes" {
+  default     = 1
+  description = "number of gke nodes"
+}
